26eaaffb872d7046be9417ae53302e59dbc7b808
Kevin <aoxianglele2010@gmail.com>
2017-09-25 15:34:26 +0800

    Add Image Enhance for generated image.

--- TrainingDataGenerator/Scripts/generateNumberImage.py
+++ TrainingDataGenerator/Scripts/generateNumberImage.py
@@ -2,7 +2,7 @@
 import threading
 import os
 import shutil
-from PIL import Image, ImageDraw2, ImageDraw, ImageFont
+from PIL import Image, ImageDraw2, ImageDraw, ImageFont, ImageEnhance
 import random
 
 count = range(0, 200)
@@ -24,7 +24,13 @@ def create_image(idx):
     drawBrush = ImageDraw.Draw(o_image)
     drawBrush.text((100 + random.randint(-30, 30), 20 + random.randint(-5, 5)), text, fill='black', font=ImageFont.truetype('./OCR-B 10 BT.ttf', 20 + random.randint(-5, 5)))
 
-    o_image.rotate(random.randint(-2, 2)).save(path + '/%d.png' % idx)
+    o_image = ImageEnhance.Color(o_image).enhance(random.uniform(0, 2)) # 着色
+    o_image = ImageEnhance.Brightness(o_image).enhance(random.uniform(0.3, 2)) #亮度
+    o_image = ImageEnhance.Contrast(o_image).enhance(random.uniform(0.2, 2)) # 对比度
+    o_image = ImageEnhance.Sharpness(o_image).enhance(random.uniform(0.2, 3.0)) #对比度
+    o_image = o_image.rotate(random.randint(-2, 2))
+
+    o_image.save(path + '/%d.png' % idx)
 
 if __name__ == '__main__':
     start()
